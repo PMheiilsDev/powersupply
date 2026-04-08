9fdf7170f15cbe0108d5c36a6cfadb185789eafb
PM <>
2026-04-08 08:07:05 +0000

    moved types from decoder to decoder_types

--- decoder/decoder.h
+++ decoder/decoder.h
@@ -1,88 +1,14 @@
 #pragma once
 
 #include <inttypes.h>
-// #include "spi_capture.h"
-
-typedef enum ROW
-{
-    VOLTAGE = 0,
-    CURRENT,
-    POWER,
-    ROW_LEN
-} 
-row_t;
-
-typedef enum DIGIT 
-{
-    DIGIT_0 = 0,    // leftmost
-    DIGIT_1,        // left middle
-    DIGIT_2,        // right middle
-    DIGIT_3,        // rightmost
-    DIGIT_LEN
-} 
-digit_t;
-
-typedef enum SEGMENT
-{
-    SEGMENT_A = 0,
-    SEGMENT_B,
-    SEGMENT_C,
-    SEGMENT_D,
-    SEGMENT_E,
-    SEGMENT_F,
-    SEGMENT_G,
-    SEGMENT_DP,
-    SEGMENT_LEN
-} 
-segment_t;
-
-typedef struct 
-{
-    uint8_t byte;
-    uint8_t bit;
-} 
-bit_position_t;
+#include "decoder_types.h"
 
 #define NUM_TO_BIT_POS_T(n)  { .byte = (n)/8, .bit = (n)%8 }
 
 extern bit_position_t data_mapping[ROW_LEN][DIGIT_LEN][SEGMENT_LEN];
 
-typedef enum BCD_TO_SEGMENT
-{
-    SEG_A = (1u << SEGMENT_A),
-    SEG_B = (1u << SEGMENT_B),
-    SEG_C = (1u << SEGMENT_C),
-    SEG_D = (1u << SEGMENT_D),
-    SEG_E = (1u << SEGMENT_E),
-    SEG_F = (1u << SEGMENT_F),
-    SEG_G = (1u << SEGMENT_G),
-    SEG_DP = (1u << SEGMENT_DP),
-}
-bcd_to_segment_t;
-
-
-typedef struct
-{
-    // all values are given in std SI units (V,A,W) 
-    // where value = fac * 10**exp;  
-    uint16_t fac;
-    int8_t exp;
-} value_t;
-
 
 extern const uint8_t segment_mapping[];
-
-typedef struct
-{
-    value_t rows[ROW_LEN];
-
-    // uint8_t current;
-    // uint8_t power;
-
-    row_t editing_row;
-    digit_t editing_digit;
-}
-screendata_t;
 
 extern screendata_t screendata;
 
