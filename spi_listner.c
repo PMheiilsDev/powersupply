ddcd2e3e049ae21149b6d5afe09eef701f20e30e
PM <>
2026-05-11 08:54:32 +0000

    added test for changing voltage with debugger

--- spi_listner.c
+++ spi_listner.c
@@ -3,13 +3,17 @@
 #include "spi_capture.h"
 #include "decoder.h"
 #include "pico/multicore.h"
+#include "controller.h"
 
+volatile int target = 0;
 
 
 int main()
 {
     stdio_init_all();
 
+    spi_capture_init();
+    controller_init();
 
     multicore_launch_core1(lambda_capture_callback);    
 
@@ -20,6 +24,20 @@ int main()
         // printf("position editing: row=%d digit=%d\n", screendata.editing_row, screendata.editing_digit);
 
         sleep_ms(50);
+        
+        // reach target
+        int distance = target - screendata.rows[VOLTAGE].fac;
+
+        if (distance < 0)
+        {
+            printf("moving down by %d", -distance );
+            rotate_digit_voltage(false, -distance);
+        }
+        else if ( distance > 0)
+        {
+            printf("moving up by %d", distance );
+            rotate_digit_voltage(true, distance);
+        }
     }
 
 }
